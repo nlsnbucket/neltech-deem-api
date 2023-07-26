1b7d8cb34972604cf09d1055af78120706e1477f
jotaviobueno <joseotaviocbueno@gmail.com>
2023-07-26 16:49:04 -0300

    add more test

--- src/modules/order/order.service.spec.ts
+++ src/modules/order/order.service.spec.ts
@@ -11,6 +11,7 @@ import {
   CustomerOrderEntity,
   OrderEntity,
 } from 'src/domain/entities';
+import { HttpException } from '@nestjs/common';
 
 describe('OrderService', () => {
   let service: OrderService;
@@ -45,6 +46,33 @@ describe('OrderService', () => {
       id: 1,
       customerId: 1,
       orderId: 1,
+    },
+  ];
+
+  const manyOrders = [
+    {
+      id: 1,
+      reason: 'Creation of site',
+      description: 'n/a',
+      offer: 5012,
+      createdAt: '2023-07-26T19:40:05.712Z',
+      updatedAt: '2023-07-26T19:40:05.712Z',
+      deletedAt: null,
+      CustomerOrder: [
+        {
+          id: 1,
+          customerId: 1,
+          orderId: 1,
+          customer: {
+            id: 1,
+            phoneNumber: '119305767321',
+            email: null,
+            createdAt: '2023-07-26T19:40:05.544Z',
+            updatedAt: '2023-07-26T19:40:05.544Z',
+            deletedAt: null,
+          },
+        },
+      ],
     },
   ];
 
@@ -97,5 +125,74 @@ describe('OrderService', () => {
 
     expect(spyCreate).toHaveBeenCalledTimes(1);
     expect(createSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('findAll', async () => {
+    const responseMock = manyOrders;
+
+    const findSpy = jest
+      .spyOn(prismaService.order as any, 'findMany')
+      .mockResolvedValue(responseMock);
+
+    const spyFind = jest.spyOn(service, 'findAll');
+    const response = await service.findAll({ page: 1, per_page: 10 });
+
+    expect(response).toStrictEqual(responseMock);
+    expect(findSpy).toHaveBeenCalledWith({
+      where: {
+        deletedAt: null,
+      },
+      include: {
+        CustomerOrder: {
+          include: {
+            customer: true,
+          },
+        },
+      },
+      skip: (1 - 1) * 10,
+      take: 10,
+    });
+
+    expect(spyFind).toHaveBeenCalledTimes(1);
+    expect(findSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('findOne', async () => {
+    const responseMock = manyOrders[0];
+
+    const findSpy = jest
+      .spyOn(prismaService.order as any, 'findFirst')
+      .mockResolvedValue(responseMock);
+
+    const spyFind = jest.spyOn(service, 'findOne');
+    const response = await service.findOne(1);
+
+    expect(response).toStrictEqual(responseMock);
+    expect(findSpy).toHaveBeenCalledWith({
+      where: {
+        id: 1,
+        deletedAt: null,
+      },
+      include: {
+        CustomerOrder: {
+          include: {
+            customer: true,
+          },
+        },
+      },
+    });
+
+    expect(spyFind).toHaveBeenCalledTimes(1);
+    expect(findSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should throw an error when not found order', async () => {
+    jest.spyOn(prismaService.order, 'findFirst').mockResolvedValue(null);
+
+    const spyFind = jest.spyOn(service, 'findOne');
+
+    await expect(service.findOne(1)).rejects.toThrow(HttpException);
+
+    expect(spyFind).toHaveBeenCalledTimes(1);
   });
 });
